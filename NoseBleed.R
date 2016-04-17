b439b262461b33fb968b73d9fb30ebf70e927fee
bravedream <bravedream850131@gmail.com>
2016-04-17 17:35:34 -0400

    Added final segmentation graphs by prediction methods
    Cleaned up univariate and bi-variate exploratory
    Added segmentation graphs for ML and GLM predicted nose bleeds

--- NoseBleed.R
+++ NoseBleed.R
@@ -39,11 +39,12 @@ apply(dat, 2, function(x) sum(is.na(x))) ##found 60 NAs in eye color, which is f
 #impute/re-categorize missing
 dat[is.na(dat$eye.colour),"eye.colour"]<-"UNKNOWN"   #keep the NAs and replace with unknown to avoid losing 60 sample
 dat[is.na(dat$mucus.viscosity),"mucus.viscosity"]<-median(dat[,"mucus.viscosity"], na.rm=T)  #simple impute the median for the missing
+
 #check summary statistics
 summary(dat)  
 apply(dat, 2, function(x) sum(is.na(x)))  #recheck, no missing
 names(dat)[names(dat)=="previous.year"]="hosp.bleed"  #change to more meaningful name for hospitalization due to bleed
-
+#descriptive 
 piefun<-function(x,title) {
 	lbls<-paste(names(table(x)),round(prop.table(table(x))*100), "%")
 	pie(table(x), label=lbls, main=title)
@@ -53,7 +54,7 @@ piefun(dat$arm,"Treatment Arms")
 piefun(dat$country,"Country")
 piefun(dat$eye.colour, "Eye Colour")
 piefun(dat$tissue.use, "Baseline Tissue Usage")
-boxplot(dat$nosebleeds,dat$hosp.bleed,dat$mucus.viscosity)
+par(mfrow=c(1,1))
 p1<-ggplot(dat,aes(x=nosebleeds))+geom_density(alpha=0.5)+ggtitle("Number of Nose Bleeds")
 p2<-ggplot(dat,aes(x=duration))+geom_density(alpha=0.5)+ggtitle("Trial Duration of Follow Up")
 p3<-ggplot(dat,aes(x=hosp.bleed))+geom_density(alpha=0.5)+ggtitle("Number of Hosp from Bleeds Previous Year")
@@ -74,7 +75,7 @@ apply(dat[,c("country","eye.colour","tissue.use")],2,function(x) summary(table(d
 scatterplotMatrix(~nosebleeds+hosp.bleed+duration+mucus.viscosity|arm, by.group=T,lower.panel=NULL,legend.pos="center", data=dat,main="")
 #by treatment difference
 summary(lm(nosebleeds~arm,data=dat))
-round(wilcox.test(nosebleeds ~arm,data=dat)$p.value,4) #viscosity<=25 quartile
+pval=round(wilcox.test(nosebleeds ~arm,data=dat)$p.value,4) #viscosity<=25 quartile
 d<-ggplot(dat,aes(x=nosebleeds))
 d+geom_density(aes(group=arm, colour=arm, fill=arm),size=2, alpha=0.3)+ggtitle("Number of Nose Bleeds by Arm")+ annotate("text", x = 4, y = 1, label=paste0("p value: ",pval))
 
@@ -82,10 +83,10 @@ d+geom_density(aes(group=arm, colour=arm, fill=arm),size=2, alpha=0.3)+ggtitle("
 wilcox.test(nosebleeds ~arm,data=dat[dat$mucus.viscosity<=summary(dat$mucus.viscosity)[[2]],]) #viscosity<=25 quartile
 wilcox.test(nosebleeds ~arm,data=dat[dat$mucus.viscosity>summary(dat$mucus.viscosity)[[2]] & dat$mucus.viscosity<=summary(dat$mucus.viscosity)[[5]],])  #viscosity between 25 and 75 quartile
 wilcox.test(nosebleeds ~arm,data=dat[dat$mucus.viscosity>summary(dat$mucus.viscosity)[[5]],])  #viscosity above 75 quartile
-xyplot(nosebleeds ~ mucus.viscosity, group=arm, data=dat, 
-       auto.key=list(corner = c(0.95,0.9 ), pch=10,cex=1.0), 
-       jitter.x=TRUE, jitter.y=TRUE,type = c("p","r"), main="Bleeds and Mucus Viscosity" )
 summary(lm(nosebleeds~arm+mucus.viscosity+arm:mucus.viscosity,data=dat))
+
+p <- ggplot(dat, aes(mucus.viscosity, nosebleeds,color=arm))
+p + geom_point()+geom_smooth(method="lm", fill=NA)+ggtitle("Bleeds and Mucus Viscosity")
 
 #bleed to tissue use by arm
 pvalM<-round(wilcox.test(nosebleeds~arm,data=dat[dat$tissue.use=="MEDIUM",])$p.value,4)
@@ -100,13 +101,19 @@ wilcox.test(nosebleeds~arm,data=dat[dat$eye.colour=="BLACK",])
 wilcox.test(nosebleeds~arm,data=dat[dat$eye.colour=="BROWN",])
 wilcox.test(nosebleeds~arm,data=dat[dat$eye.colour=="UNKNOWN",])
 d<-ggplot(dat,aes(x=nosebleeds))
-d+geom_density(aes(group=arm, colour=arm, fill=arm),size=2, alpha=0.3)+facet_wrap(~eye.colour,nrow=2)
-#bleed by country by arm
+d+geom_density(aes(group=arm, colour=arm, fill=arm),size=2, alpha=0.3)+facet_wrap(~eye.colour,nrow=2)+ggtitle("Nose Bleeds by Eye Color")
 
+#bleed by country by arm
 wilcox.test(nosebleeds~arm,data=dat[dat$country=="A",])
-wilcox.test(nosebleeds~arm,data=dat[dat$eye.colour=="BLACK",])
-wilcox.test(nosebleeds~arm,data=dat[dat$eye.colour=="BROWN",])
-wilcox.test(nosebleeds~arm,data=dat[dat$eye.colour=="UNKNOWN",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="B",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="C",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="D",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="E",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="F",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="G",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="H",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="I",])
+wilcox.test(nosebleeds~arm,data=dat[dat$country=="J",])
 
 d<-ggplot(dat,aes(x=nosebleeds))
 d+geom_density(aes(group=arm, colour=arm, fill=arm),size=2, alpha=0.3)+facet_wrap(~country,nrow=2)+ggtitle("Nose Bleeds by country")
@@ -117,7 +124,7 @@ wilcox.test(nosebleeds ~arm,data=dat[dat$hosp.bleed==2,]) #hospitalization lack 
 #hospitalization due to nose bleed by country
 kruskal.test(hosp.bleed ~ country, data = dat)   #non parametric test (kruskal) of between country hospitalization
 d<-ggplot(dat,aes(x=hosp.bleed))
-d+geom_density(aes(group=country, colour=country, fill=country), alpha=0.3)+facet_wrap(~country,ncol= nlevels(dat$country))+ggtitle("Hospitalizations by Country")
+d+geom_density(aes(group=country, colour=country, fill=country), alpha=0.3)+facet_wrap(~country,ncol= nlevels(dat$country))+xlab("Hospitalization")+ggtitle("Hospitalizations by Country")
 
 round(prop.table(table(dat$hosp.bleed,dat$country)*100),2)
 aggregate(. ~ country,data = dat[,c("country","hosp.bleed")], FUN=function(x) c(mean =round(mean(x),2), SD=round(sd(x),2)))  #extract mean and SD
@@ -125,7 +132,7 @@ aggregate(. ~ country,data = dat[,c("country","hosp.bleed")], FUN=function(x) c(
 ##########################
 #Prediction Models
 ##########################
-##bleed prediction\
+##bleed prediction
 mean(dat$nosebleeds)
 var(dat$nosebleeds)   #overdispersed (mean<variance)
 #negative binomial   
@@ -133,7 +140,7 @@ summary(nb <- glm.nb(nosebleeds ~  arm +offset(log(duration))+ hosp.bleed+tissue
 	+arm:mucus.viscosity+hosp.bleed:mucus.viscosity+arm:hosp.bleed
 	,data = dat,link=log))
 prednb<-exp(predict(nb,dat)) ##predict number of events
-plot(dat$nosebleeds,prednb)
+plot(dat$nosebleeds,prednb, main="Nose Bleeds (observed vs. NB predicted)",xlab="Observed", ylab="NB Predicted")
 sqrt(sum((prednb-dat$nosebleeds)^2)/nrow(dat))
 summary(prednb-dat$nosebleeds)
 
@@ -147,7 +154,11 @@ plot(dat$nosebleeds,predzinb)
 sqrt(sum((predzinb-dat$nosebleeds)^2)/nrow(dat))
 vuong(update(zinb,.~1),zinb)  #test if intercept-only zero part model is sig. different than the zero model with tissue--answer is yes
 vuong(zinb,nb)  #test if there is significant difference between standard negative binomial and zero-inflated model--answer is very marginally
-
+#plot compare predictive vs. observed
+par(mfrow=c(2,1))
+hist(prednb, main="Neg Binomial Predicted Bleeds", xlab="Predicted")
+hist(dat$nosebleeds,main="Observed Bleeds", xlab="Observed")
+par(mfrow=c(1,1))
 #try standard poisson model 
 summary(zip <- zeroinfl(nosebleeds ~ arm + offset(log(duration))+hosp.bleed+tissue.use+mucus.viscosity+country
 	+arm:mucus.viscosity+hosp.bleed:mucus.viscosity
@@ -183,39 +194,42 @@ str(testing)
 #random forest
 rfFit <- train(nosebleeds~., 
 		data=training,method = "rf",importance = TRUE,
-		trControl=trainControl(method = "oob"))
+		trControl=trainControl(method = "oob")
+		,preProc = c("center", "scale"))
 rfFit$finalModel
 plot(rfFit$finalModel)
 rfPred<-predict(rfFit,testing)
-plot(testing$nosebleeds,rfPred,xlim=c(0,max(testing$nosebleeds)),ylim=c(0,max(rfPred)),main="Random Forest")
+plot(testing$nosebleeds,rfPred,xlim=c(0,max(testing$nosebleeds)),ylim=c(0,max(rfPred)),main="Random Forest", ylab="Predictied", xlab="Observed")
 RMSErf<-sqrt(sum((rfPred-testing$nosebleeds)^2)/nrow(testing))
 RMSErf
 plot(varImp(rfFit,scale=TRUE),main="Random Forest")
 
 #gradient boost machine
-gbmControl <- trainControl(## k-fold CV
+gbmControl <- trainControl(## 5-fold repeated CV
                            method = "repeatedcv",
                            number = 5,
                            ## repeated x times
                            repeats = 5)
 gbmGrid <-  expand.grid(interaction.depth = (1:10)*1,
                         n.trees = (1:30)*3,
-                        shrinkage =0.1,
-                        n.minobsinnode = 10)
+                        shrinkage =0.1,  #tried 0.05 to 1. To save computation time, set to optimal 0.1
+                        n.minobsinnode = 10)  #tried 5 to 20. To save computation time, set to optimal 10
+
 gbmFit1 <- train(nosebleeds~., 
 		data=training,
             method = "gbm",
+		distribution = "poisson",
            	trControl = gbmControl,
 		tuneGrid=gbmGrid,
             verbose = FALSE
-#		,preProc = c("center", "scale")
+		,preProc = c("center", "scale")
 )
 gbmFit1
 gbmPred=predict(gbmFit1,testing)
 RMSEgbm<-sqrt(sum((gbmPred-testing$nosebleeds)^2)/nrow(testing))
 RMSEgbm
-plot(testing$nosebleeds,gbmPred,xlim=c(0,max(testing$nosebleeds)),ylim=c(0,max(gbmPred)))
-plot(varImp(gbmFit1,scale=TRUE),main="Gradient Bossting Machine")
+plot(testing$nosebleeds,gbmPred,xlim=c(0,max(testing$nosebleeds)),ylim=c(0,max(gbmPred)),main="Gradient Boosting Machine",ylab="Predictied", xlab="Observed")
+plot(varImp(gbmFit1,scale=TRUE),main="Gradient Boosting Machine")
 ggplot(gbmFit1)
 
 
@@ -237,54 +251,156 @@ svmFit$finalModel
 svmPred=predict(svmFit,testing)
 RMSEsvm<-sqrt(sum((svmPred-testing$nosebleeds)^2)/nrow(testing))
 RMSEsvm
-plot(testing$nosebleeds,svmPred,xlim=c(0,max(testing$nosebleeds)),ylim=c(0,max(svmPred)))
-plot(varImp(svmFit,scale=TRUE))
+plot(testing$nosebleeds,svmPred,xlim=c(0,max(testing$nosebleeds)),ylim=c(0,max(svmPred)),main="Support Vector Machine",ylab="Predictied", xlab="Observed")
 ggplot(svmFit)
 
-##ensemble  of GBM, Random Forest 
-combine<-as.data.frame(cbind(gbmPred,rfPred))
-combineN<-cbind(gbmPred,rfPred)
+##ensemble  of GBM, Random Forest ,svm
+combine<-as.data.frame(cbind(gbmPred,rfPred,svmPred))
+combineN<-cbind(gbmPred,rfPred,svmPred)
 cor(combineN)
 combine$avg<-apply(combine,1,mean) 
-sqrt(sum((combine$avg-testing$nosebleeds)^2)/nrow(testing))
-plot(testing$nosebleeds,combine$avg,xlim=c(0,max(testing$nosebleeds)),ylim=c(0,max(combine$avg)))
+sqrt(sum((combine$avg-testing$nosebleeds)^2)/nrow(testing)) #unit weight average brings slight improvement, non worse than any single model
+#find the optimal weight by minimizing sum of square on 
+target<-function(x) {
+	combine$avg<-x[1]*gbmPred+x[2]*rfPred+(1-x[1]-x[2])*svmPred
+	ensnblRMSE<-sqrt(sum((combine$avg-testing$nosebleeds)^2)/nrow(testing))
+}
+pstart<-c(0.33,0.33)
+optimWt<-optim(pstart,target,method = "L-BFGS-B",lower=c(0,0),upper=c(1,1))
+optimWt  
+wtAvg<-optimWt$par[1]*gbmPred+optimWt$par[2]*rfPred+(1-optimWt$par[1]-optimWt$par[2])*svmPred
+plot(testing$nosebleeds,wtAvg, 
+	xlab="Observed Nose Bleeds",ylab="Predicted", main="Ensemble Prediction of Nose Bleeds in Validation Sample")
 
-
+##########################################
+#Final solutions for patient segmentation
+##########################################
 #extract distribution (percentiles) of mucus viscosity
 viscosity_q<-as.data.frame(quantile(dat$mucus.viscosity,seq(0,1,0.1)))
 
-#From the Negative Binomial Model, calculate mean predicted treatment effect (reduction of bleeds) by percentile/cumulatively of viscosity
+##From the Negative Binomial Model, calculate mean predicted treatment effect (reduction of bleeds) by percentile/cumulatively of viscosity
+cut<-50
+step<-(max(dat$mucus.viscosity)-min(dat$mucus.viscosity))/(cut-1)
+seq=seq(from=max(dat$mucus.viscosity), to=min(dat$mucus.viscosity), by=-step)
 TxbyVisc<-data.frame()
-for (i in 1:10) {
+for (i in 1:(cut-1)) {
 	TxbyVisc<-rbind(TxbyVisc,
-		as.data.frame(cbind(paste0("Top ", i*10,"%"),as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$mucus.viscosity<=viscosity_q[[1]][12-i],]))[[1]])))
+		as.data.frame(cbind(as.character(round(seq[i],2)),as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$mucus.viscosity<=seq[i],]))[[1]])))
 	)	
 }
-names(TxbyVisc)[1]<-"Top x%"
-plot(TxbyVisc[,1],TxbyVisc$diff,
-    ylim=range(c(TxbyVisc$lwr, TxbyVisc$upr)),
-    pch=19, xlab="Segment by Mucus Viscosity", ylab="Mean Predicted Reduction (Active-Placebo) ",
-    main="Predicted Bleeds Reduction by Mucus Viscosity"
-)
-arrows(c(1:10), TxbyVisc$lwr, c(1:10), TxbyVisc$upr, length=0.05, angle=90, code=3)
+names(TxbyVisc)[1]<-"Viscosity_Threshold"
+TxbyVisc$popSize<-apply(as.data.frame(seq),1,function(x) nrow(dat[dat$mucus.viscosity>=x,])/nrow(dat))[1:(length(seq)-1)]
+par(mar = c(5,5,2,5))
+with(TxbyVisc,plot(as.numeric(Viscosity_Threshold),diff,
+    	ylim=range(c(lwr,upr)),type="o",pch=19,las = 2, xaxt="n",
+	xlab="Mucus Viscosity Threshold (>)", ylab="Mean Predicted Reduction (Active-Placebo) ",
+    	main="GLM-Predicted Bleed Rate Reduction by Mucus Viscosity"
+))
+axis(1,at=1:(length(seq)-1), labels=TxbyVisc$Viscosity_Threshold,las=2)
+#add confidence intervals
+arrows(c(1:(cut-1)), TxbyVisc$lwr, c(1:(cut-1)), TxbyVisc$upr, length=0.05, angle=90, code=3)
 abline(h=0,lwd=2,col="red")
 
-#Repeat the plot for the Machine Learning-Predicted (ensemble) 
+#add secondary y axis for population size
+par(new = T)
+plot(as.numeric(TxbyVisc[,1]), TxbyVisc$popSize,type="o", pch=15, col="blue",axes=F, xlab=NA, ylab=NA, cex=1.2)
+axis(side = 4)
+mtext(side = 4, line = 3, 'Population Size')
+legend("topleft",
+       legend=c("Bleed Rate Reduction", "Population Size"),
+       cex=0.9, pch=c(19, 15), col=c("black", "blue"))
+#repeat for segmentation by tissue use
+TxbyTissue<-data.frame()
+TxbyTissue<-as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$tissue.use=="MEDIUM",]))[[1]])
+TxbyTissue<-rbind(TxbyTissue,	as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$tissue.use=="HIGH",]))[[1]]))
+TxbyTissue$Tissue<-c("Medium","High")
+barplot(TxbyTissue$diff,ylim=range(c(-0.5,0.5)),xlab="Tissue Usage",ylab="Mean Predicted Reduction (Active-Placebo) ",
+	names.arg=TxbyTissue$Tissue, main="GLM-Predicted Bleeds Reduction by Tissue Usage")
+arrows(c(1:2), TxbyTissue$lwr, c(1:2), TxbyTissue$upr, length=0.05, angle=90, code=3)
+abline(h=0,lwd=2,col="red")
+
+#repeat for segmentation by previous hospitalization
+TxbyHosp<-data.frame()
+TxbyHosp<-as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$hosp.bleed==2,]))[[1]])
+TxbyHosp<-rbind(TxbyHosp,	as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$hosp.bleed>2,]))[[1]]))
+TxbyHosp$Tissue<-c("=2",">2")
+barplot(TxbyHosp$diff,ylim=range(c(-0.5,0.5)),xlab="Prev. Hosp.",ylab="Mean Predicted Reduction (Active-Placebo) ",
+	names.arg=TxbyHosp$Tissue, main="GLM-Predicted Bleeds Reduction by Previous Hospitalizations")
+arrows(c(1:2), TxbyHosp$lwr, c(1:2), TxbyHosp$upr, length=0.05, angle=90, code=3)
+abline(h=0,lwd=2,col="red")
+
+##Repeat the plot for the Machine Learning-Predicted (ensemble) 
+#re-run 3 machine learning algorithms on full sample to create complete inference of treatment effect stratified by indicators (viscosity, etc)
+gbmFull<-predict(gbmFit1,dat)
+rfFull<-predict(rfFit,dat)
+svmFull<-predict(svmFit,dat)
+gbmFullRMSE<-sqrt(sum((gbmFull-dat$nosebleeds)^2)/nrow(dat))
+rfFullRMSE<-sqrt(sum((rfFull-dat$nosebleeds)^2)/nrow(dat))
+svmfFullRMSE<-sqrt(sum((svmFull-dat$nosebleeds)^2)/nrow(dat))
+#ensemble on the full sample
+combine<-as.data.frame(cbind(gbmFull,rfFull,svmFull))
+combineN<-cbind(gbmFull,rfFull,svmFull)
+cor(combineN)
+
+target<-function(x) {
+	combine$avg<-x[1]*gbmFull+x[2]*rfFull+(1-x[1]-x[2])*svmFull
+	ensnblRMSE<-sqrt(sum((combine$avg-dat$nosebleeds)^2)/nrow(dat))
+	return(ensnblRMSE)
+}
+pstart<-c(0.33,0.33)
+optimWt<-optim(pstart,target,method = "L-BFGS-B",lower=c(0,0),upper=c(1,1))
+optimWt  
+dat$finalML<-optimWt$par[1]*gbmFull+optimWt$par[2]*rfFull+(1-optimWt$par[1]-optimWt$par[2])*svmFull
+plot(dat$nosebleeds,dat$finalML, 
+	xlab="Observed Nose Bleeds",ylab="Predicted", main="Ensemble Prediction of Nose Bleeds in Full Sample")
+
 TxbyViscML<-data.frame()
-for (i in 1:10) {
+for (i in 1:(cut-1)) {
 	TxbyViscML<-rbind(TxbyViscML,
-		as.data.frame(cbind(paste0("Top ", i*10,"%"),as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$mucus.viscosity<=viscosity_q[[1]][12-i],]))[[1]])))
+		as.data.frame(cbind(as.character(round(seq[i],2)),as.data.frame(TukeyHSD(aov(finalML~arm,data=dat[dat$mucus.viscosity<=seq[i],]))[[1]])))
 	)	
 }
-names(TxbyViscML)[1]<-"Top x%"
-plot(TxbyViscML[,1],TxbyViscML$diff,
-    ylim=range(c(TxbyViscML$lwr, TxbyViscML$upr)),
-    pch=19, xlab="Segment by Mucus Viscosity", ylab="Mean Predicted Reduction (Active-Placebo) ",
-    main="Predicted Bleeds Reduction by Mucus Viscosity"
-)
-arrows(c(1:10), TxbyViscML$lwr, c(1:10), TxbyViscML$upr, length=0.05, angle=90, code=3)
+names(TxbyViscML)[1]<-"Viscosity_Threshold"
+TxbyViscML$popSize<-apply(as.data.frame(seq),1,function(x) nrow(dat[dat$mucus.viscosity>=x,])/nrow(dat))[1:(length(seq)-1)]
+par(mar = c(5,5,2,5))
+with(TxbyViscML,plot(as.numeric(Viscosity_Threshold),diff,
+    	ylim=range(c(lwr,upr)),type="o",pch=19,las = 2, xaxt="n",
+	xlab="Mucus Viscosity Threshold (>)", ylab="Mean Predicted Reduction (Active-Placebo) ",
+    	main="ML-Predicted Bleed Rate Reduction by Mucus Viscosity"
+))
+axis(1,at=1:(length(seq)-1), labels=TxbyViscML$Viscosity_Threshold,las=2)
+#add confidence intervals
+arrows(c(1:(cut-1)), TxbyViscML$lwr, c(1:(cut-1)), TxbyViscML$upr, length=0.05, angle=90, code=3)
 abline(h=0,lwd=2,col="red")
 
+#add secondary y axis
+par(new = T)
+plot(as.numeric(TxbyViscML[,1]), TxbyViscML$popSize,type="o", pch=15, col="blue",axes=F, xlab=NA, ylab=NA, cex=1.2)
+axis(side = 4)
+mtext(side = 4, line = 3, 'Population Size')
+legend("topleft",
+       legend=c("Bleed Rate Reduction", "Population Size"),
+       cex=0.9, pch=c(19, 15), col=c("black", "blue"))
+
+#repeat for tx effect by tissue use
+TxbyTissueML<-data.frame()
+TxbyTissueML<-as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$tissue.use=="MEDIUM",]))[[1]])
+TxbyTissueML<-rbind(TxbyTissueML,	as.data.frame(TukeyHSD(aov(finalML~arm,data=dat[dat$tissue.use=="HIGH",]))[[1]]))
+TxbyTissueML$Tissue<-c("Medium","High")
+barplot(TxbyTissueML$diff,ylim=range(c(-0.5,0.5)),xlab="Tissue Usage",ylab="Mean Predicted Reduction (Active-Placebo) ",
+	names.arg=TxbyTissueML$Tissue, main="ML-Predicted Bleeds Reduction by Tissue Usage")
+arrows(c(1:2), TxbyTissueML$lwr, c(1:2), TxbyTissueML$upr, length=0.05, angle=90, code=3)
+abline(h=0,lwd=2,col="red")
+
+#repeat for tx effect by tissue use
+TxbyHospML<-data.frame()
+TxbyHospML<-as.data.frame(TukeyHSD(aov(nbPred~arm,data=dat[dat$hosp.bleed==2,]))[[1]])
+TxbyHospML<-rbind(TxbyHospML,	as.data.frame(TukeyHSD(aov(finalML~arm,data=dat[dat$hosp.bleed>2,]))[[1]]))
+TxbyHospML$Tissue<-c("=2",">2")
+barplot(TxbyHospML$diff,ylim=range(c(-0.5,0.5)),xlab="Prev. Hosp.",ylab="Mean Predicted Reduction (Active-Placebo) ",
+	names.arg=TxbyHospML$Tissue, main="ML-Predicted Bleeds Reduction by Previous Hospitalizations")
+arrows(c(1:2), TxbyHospML$lwr, c(1:2), TxbyHospML$upr, length=0.05, angle=90, code=3)
+abline(h=0,lwd=2,col="red")
 
 
 
